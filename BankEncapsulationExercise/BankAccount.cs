ab2768f7dc82d556b25794bca28e7e3697c89d21
Alvaro11G <agarcia11746@gmail.com>
2022-09-21 11:26:54 -0600

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        public void deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public int amount { get; set; }
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine("Enter a amount you want to deposit.");
+            int accountInput = Convert.ToInt32(Console.ReadLine());
+
+            account.amount = accountInput;
+
+            Console.WriteLine($"The amount in your bank account is: ${account.amount}");
+        }
+    }
+}
